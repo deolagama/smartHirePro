58b9c39bec92f2937b1892b2f0c4aa6e46612428
deolagama <deolagamaa@gmail.com>
2025-10-14 19:25:44 +0000

    Extraction from image

--- src/preprocess_dataset.py
+++ src/preprocess_dataset.py
@@ -1,20 +1,35 @@
-import fitz 
+import fitz
 import re
+import io
+from PIL import Image
+import pytesseract
 from datasets import load_dataset
 from langdetect import detect, DetectorFactory
 
-DetectorFactory.seed = 0 #It removes randomness from language detection
+DetectorFactory.seed = 0
+
 ds = load_dataset("d4rk3r/resumes-raw-pdf")
 
-def extract_text_from_pdf_bytes(pdf_bytes):
+def extract_text_from_pdf_or_image(pdf_bytes):
     text = ""
-    with fitz.open(stream=pdf_bytes, filetype="pdf") as doc: #resumes come from a dataset in memory, not local files
-        for page in doc:
-            text += page.get_text("text")
-    return text
+    try:
+        with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
+            for page in doc:
+                text += page.get_text("text")
+            if text.strip():
+                return text
+    except Exception as e:
+        pass  
+    try:
+        image = Image.open(io.BytesIO(pdf_bytes)) #Convert raw bytes → in-memory file,uses Pillow (PIL) to open that in-memory file as an image.
+        text = pytesseract.image_to_string(image) #eads the image and uses machine learning to detect and extract the text printed in it
+        return text
+    except Exception as e:
+        return ""
+
 #We use PDF bytes because it lets us read and process PDFs directly from memory, without saving files, making the workflow faster, scalable, and cleaner .
 def preprocess_pdf(example): #Each dictionary (row) represents one resume in your case.
-    pdf_bytes = example['content']  
+    pdf_bytes = example["pdf"] 
     text = extract_text_from_pdf_bytes(pdf_bytes)
     example['raw_text'] = text
     return example
@@ -28,7 +43,7 @@ def clean_text(text):
     text = re.sub(r'\s+', ' ', text).strip()  # normalize spaces
     return text.lower()
 def preprocess_pdf(example):
-    pdf_bytes = example["content"]
+    pdf_bytes = example["pdf"]
     raw_text = extract_text_from_pdf_bytes(pdf_bytes)
     if not raw_text or len(raw_text) < 50:
         example["keep"] = False
@@ -50,6 +65,6 @@ def preprocess_pdf(example):
 
 ds = ds.map(preprocess_pdf)
 ds_filtered = ds.filter(lambda x: x["keep"])
-ds_filtered.save_to_disk("processed_resumes_en")
+ds_filtered.save_to_disk("../data/processed_resumes_en")
 
 print("Preprocessing complete, saved English resumes to 'processed_resumes_en/'")
